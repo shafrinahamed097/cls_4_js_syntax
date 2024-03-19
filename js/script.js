0893a05a4e190e33019de426bc5839d7097b1937
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-03-19 14:34:11 +0600

    Declare function, Array, Non-Primitive Data type

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let name = "John Doe";
+name = "Mr John Doe";
+console.log(name);
+
+// let n1 = 16;
+// let n2 = 4;
+// let sum = n1 + n2;
+// console.log(sum);
+
+// let course = "Mastering Laravel"; // string
+// let duration = 6; // integer
+// let courseRunning = true; // boolean
+
+// console.log(course);
+// console.log(duration);
+// console.log(courseRunning);
